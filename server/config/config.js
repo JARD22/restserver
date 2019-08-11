009e820d7d6701a937f8433e12c6029a64934820
Jorge <giito16@gmail.com>
2019-08-11 00:35:47 -0600

    Restserver-Usuario Cambios en la configuracion

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ process.env.PORT = process.env.PORT || 3000;
 //=======================================
 
 
-process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //=======================================
 //              BASE DE DATOS
@@ -19,7 +19,7 @@ process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if (process.env.NODE_ENV = 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/Cafe'
 } else {
 
